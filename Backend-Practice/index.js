837c10afdab256a329892efb78b0dd0645c93fa0
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-11-11 17:49:31 +0500

    Connection with MONGO DB

---
+++ Backend-Practice/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+const PORT = 6999;
+const app = express();
+dotenv.config()
+// console.log(app)
+
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`App Listening on this Port Number ${PORT}`);
+});
+
+const connectDb = () => {
+  mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+      console.log("Backend Connected");
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
