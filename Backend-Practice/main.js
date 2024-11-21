50df2aa66254e90175fad88c8f6741163151eca1
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-11-21 19:14:10 +0500

    backend connection practice

---
+++ Backend-Practice/main.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MongoUrl = process.env.MONGOURL;
+// || 'mongodb://localhost:27017/'
+
+mongoose.connect(MongoUrl).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((error) =>{
+    console.log(error,"Error connecting to MongoDB")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
