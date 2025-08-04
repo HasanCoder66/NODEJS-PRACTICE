90485f5755cb3f97873849e13c3ac222e9d4dddc
Hasan Coder 66 <hasanashraf874@gmail.com>
2025-08-04 17:25:42 +0500

    chapter 5 done

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -27,45 +27,45 @@
 
 
 
-
 // Chapter No 2
 
 // question 1
-var username;
+// var username;
 
 // question 2
-var myName = "Muhammad Hasan Ashraf";
+// var myName = "Muhammad Hasan Ashraf";
 
 // question 3
-var message = "Hello World!";
+// var message = "Hello World!";
 // alert(message);
 
 // question 4
-var name = "Hasan";
-var age = "15 years old";
-var course = "Web & Mobile App Development";
+// var name = "Hasan";
+// var age = "15 years old";
+// var course = "Web & Mobile App Development";
 // alert(name);
 // alert(age);
 // alert(course);
 
 // question 5
-var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
 // alert(pizza)
 
 // question 6
-var email = "codermhasan@gmail.com";
+// var email = "codermhasan@gmail.com";
 // alert("My Email address is" + " " + email);
 
 // question 7
-var book = "A Smart Way to Learn JavaScript";
+// var book = "A Smart Way to Learn JavaScript";
 // alert("I am Trying to Learn from the Book" + " " + book);
 
 // question 8
 // document.writeln("Yah! I Can write HTML content through JavaScript");
 
 // question 9
-var blah = "balsjdfa;lsdj;flasjd;flkasj";
+// var blah = "balsjdfa;lsdj;flasjd;flkasj";
 // alert(blah);
+
 
 
 
@@ -76,23 +76,297 @@ var blah = "balsjdfa;lsdj;flasjd;flkasj";
 // Chapter No 3
 
 // question 1
-var age = 15;
+// var age = 15;
 // alert("I am "+ age + " " + "years old" )
 
-
 // question 2
-var visitNum = 100;
+// var visitNum = 100;
 // alert("You have visited" + " " + visitNum + " " + "Times")
 
-
 // question 3
-var birthYear = 2003;
+// var birthYear = 2003;
 // document.writeln("my birth year is " + birthYear + "<br>" + "Data type of my Declared variable is number")
 
 // question 4
-var visitorName = "Hasan";
-var productTitle = "T Shirt";
-var quantity = 78;
-var brandName = "Khaadi"
+// var visitorName = "Hasan";
+// var productTitle = "T Shirt";
+// var quantity = 78;
+// var brandName = "Khaadi";
 
-document.writeln(visitorName + " " + "Ordered " + " " + quantity + " " + productTitle +"(s)" + " "+ "on" + " "+brandName+ " " + "Clothing Store." )
+// document.writeln(visitorName + " " + "Ordered " + " " + quantity + " " + productTitle +"(s)" + " "+ "on" + " "+brandName+ " " + "Clothing Store." )
+
+
+
+
+
+
+
+
+// Chapter No 4
+
+// question 1
+// var num1 = 1,
+//   num2 = 2,
+//   num3 = 3;
+
+// console.log("num1 ===>", num1);
+// console.log("num2 ===>", num2);
+// console.log("num3 ===>", num3);
+
+
+// question 2
+
+// a 
+// Legal variables Names ====>
+// var fullName = "Muhammad Hasan"
+// var $lastName = "Ashraf"
+// var num1 = 1;
+// var my_age = 22
+// var MYUSERRESPONSE = "my user response"
+
+// b 
+// Illegal variables Names ====>
+
+// var 1st = 1;
+// var  fullName% = "hasan"
+// var *name = "hasan"
+// var my-name = "hasan" 
+// var my-3 = 56
+
+
+// question 3
+
+// a 
+// var title = "Rules for JavaScript Variables";
+
+// b
+// alert(title + " " + "$ Sign" + " " + "_ underScore " + " " + " camelCase" + " " + " number" + " " )
+
+// c 
+// alert("Variable Must begin with a" + " " + "$ Sign" + " " + "_ underScore " + " " + " name")
+
+// d 
+// var caseSensitive  = "Variable Names are Case Sensitive"
+
+// e 
+// var variableNameContains = "Variable should not be a keyword"
+
+
+
+
+
+
+
+// Chapter No 5
+
+// question 1
+
+// a
+// var num1 = 5;
+// var num2 = 7;
+
+// var sumNum = num1 + num2;
+// console.log("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sumNum);
+
+// b 
+// var num1 = 15;
+// var num2 = 7;
+
+// var sumNum = num1 - num2;
+// console.log("Subtraction of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sumNum);
+
+
+// c
+// var num1 = 15;
+// var num2 = 7;
+
+// var sumNum = num1 * num2;
+// console.log("Multiplication of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sumNum);
+
+// d
+// var num1 = 14;
+// var num2 = 7;
+
+// var sumNum = num1 / num2;
+// console.log("Division of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sumNum);
+
+// e
+// var num1 = 15;
+// var num2 = 7;
+
+// var sumNum = num1 % num2;
+// console.log("Modulus of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + sumNum);
+
+
+// question 3
+
+// a 
+// var myVariable;
+// b 
+// console.log("Value after declaration is: ",myVariable)
+// c 
+// myVariable = 22;
+// d 
+// console.log("Initial Value: ",myVariable)
+// e 
+// myVariable++
+// f 
+// console.log("Value after Increment is: ",myVariable)
+// g 
+// myVariable = myVariable + 7;
+// h 
+// console.log("Value after addition is: ",myVariable)
+// i 
+// myVariable--
+// j
+// console.log("Value after Decrement is: ",myVariable)
+// k 
+// myVariable = myVariable % 3
+// l 
+// console.log("The remainder is: ",myVariable)
+
+
+// question 4
+// var movieTicketPrize = 600;
+
+// var movies5Tickets = 5;
+
+// var totalTicketPrizes = movieTicketPrize * movies5Tickets;
+// console.log( "Total Cost to buy "+ movies5Tickets + " tickets to a movie is" + " " +totalTicketPrizes + " " + "PKR   ")
+
+
+// question 5
+// a 
+// var tableNum = 9;
+// console.log(tableNum," X 1 = ", tableNum * 1)
+// console.log(tableNum," X 2 = ", tableNum * 2)
+// console.log(tableNum," X 3 = ", tableNum * 3)
+// console.log(tableNum," X 4 = ", tableNum * 4)
+// console.log(tableNum," X 5 = ", tableNum * 5)
+// console.log(tableNum," X 6 = ", tableNum * 6)
+// console.log(tableNum," X 7 = ", tableNum * 7)
+// console.log(tableNum," X 8 = ", tableNum * 8)
+// console.log(tableNum," X 9 = ", tableNum * 9)
+// console.log(tableNum," X 10 = ",tableNum * 10)
+
+// b with the help of function
+// function tableNum (tableNum) {
+
+
+// console.log(tableNum," X 1 = ", tableNum * 1)
+// console.log(tableNum," X 2 = ", tableNum * 2)
+// console.log(tableNum," X 3 = ", tableNum * 3)
+// console.log(tableNum," X 4 = ", tableNum * 4)
+// console.log(tableNum," X 5 = ", tableNum * 5)
+// console.log(tableNum," X 6 = ", tableNum * 6)
+// console.log(tableNum," X 7 = ", tableNum * 7)
+// console.log(tableNum," X 8 = ", tableNum * 8)
+// console.log(tableNum," X 9 = ", tableNum * 9)
+// console.log(tableNum," X 10 = ",tableNum * 10)
+// }
+
+// tableNum(20)
+
+
+// question 6
+
+// var celcius = 31;
+// var fahrenheit  = (celcius * 9/5) + 32;
+
+// console.log(fahrenheit)
+
+
+
+// question 7
+// var item1 = 50;
+// var item2 = 100;
+
+// var orderedQtyOfItem1 = 2;
+// var orderedQtyOfItem2 = 5;
+
+// var shippingCharges = 100;
+
+// var item1Cost = item1 * orderedQtyOfItem1
+// var item2Cost = item2 * orderedQtyOfItem2
+
+// var totalOrderCosting = item1Cost + item2Cost + shippingCharges
+
+// console.log("Price of item 1 is " + item1)
+// console.log("Quantity of item 1 is " + orderedQtyOfItem1)
+// console.log("Price of item 2 is " + item2)
+// console.log("Quantity of item 2 is " + orderedQtyOfItem2)
+// console.log("Total Cost of Your Order "  + totalOrderCosting)
+
+
+// question 8
+
+// var totalMarks = 900;
+// var marksObtained = 899;
+
+// var percentageOfStudent = marksObtained * 100 / totalMarks;
+// console.log("Mark Sheet");
+// console.log("Total Marks:" + totalMarks);
+// console.log("Marks Obtained:" + marksObtained);
+// console.log("Student Percentage:" + percentageOfStudent);
+
+
+
+// question 9
+// a first option
+// var $dollars = 10;
+// var saudiRiyals = 25;
+
+// var one$ToPkr = 283.42;
+// var oneRiyalToPkr = 75.51;
+
+// const total$PKRCurrency = one$ToPkr * $dollars;
+// const totalRiyalPKRCurrency = oneRiyalToPkr * saudiRiyals;
+
+// var total$_riyalsIntoPKR = total$PKRCurrency + totalRiyalPKRCurrency
+// console.log("Your Total Currency into PKR is: " + total$_riyalsIntoPKR)
+
+// b second option
+// var totalPkr = (10 * 280) + (25 * 76.51)
+// console.log("Total Pkr is: " + totalPkr)
+
+
+// question 10
+// var num = 6;
+// console.log(num + 5 * 10 / 2 )
+
+
+// question 11
+
+// a 
+// var currentYear = 2025;
+// // b 
+// var birthYear = 2003;
+// // c 
+// console.log("Your Age is: ", currentYear - birthYear   )
+
+
+// question 12
+
+// var radius = 10
+// var circumference = 2 * Math.PI * radius;
+// var area = Math.PI * radius * radius;
+
+// console.log("The Radius is: " + radius);
+// console.log("The Circumference is: " + circumference.toFixed(2));
+// console.log("The Area is: " + area.toFixed(2));
+
+
+// question 13
+
+// var favSnack = "Ticket"
+// var age = 22
+// var maxAge = 42
+// var estimatedAmountPerDay = 3;
+
+// var remainingYears = maxAge - age;
+// var totalSnacksNeed = remainingYears * 365 * estimatedAmountPerDay;
+
+
+// console.log("My Favourite Snack is: " + favSnack + " my current age is: " + age + " my Estimated Age is: " + maxAge + " I need " + estimatedAmountPerDay + " snacks per day" + "you'll need " + totalSnacksNeed) 
+
+
